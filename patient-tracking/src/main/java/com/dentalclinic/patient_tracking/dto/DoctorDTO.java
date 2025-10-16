b80e7367afc0d6986cdd4b2b95060d788e04fe72
SerkanOkut <serkan.okt56@gmail.com>
2025-10-16 18:35:14 +0300

    DoctorDTO added.

---
+++ patient-tracking/src/main/java/com/dentalclinic/patient_tracking/dto/DoctorDTO.java
@@ -0,0 +1,17 @@
+package com.dentalclinic.patient_tracking.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DoctorDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String clinicName;
+}
