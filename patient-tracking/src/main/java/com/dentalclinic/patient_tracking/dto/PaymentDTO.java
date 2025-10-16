f73a9ec4c9cd46ac731f1bb179c80c99a185c22d
SerkanOkut <serkan.okt56@gmail.com>
2025-10-16 18:39:42 +0300

    PaymentDTO added.

---
+++ patient-tracking/src/main/java/com/dentalclinic/patient_tracking/dto/PaymentDTO.java
@@ -0,0 +1,19 @@
+package com.dentalclinic.patient_tracking.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentDTO {
+
+    private Long id;
+    private BigDecimal amount;
+    private LocalDateTime paymentDate;
+
+}
